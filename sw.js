ec76be953d26111fd317d09d6d7dec79a74ebed5
melipil5780-afk <melipil5780@gmail.com>
2026-02-27 09:53:25 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-// InnerShadow SW - stripped down
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-// No fetch handler = browser handles everything natively
-console.log('[SW] Minimal SW loaded');
